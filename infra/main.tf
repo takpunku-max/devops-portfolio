6e1fdd7b362bc57500a4b5a25ec6e3d2f39fe152
takpunku-max <takpunku@gmail.com>
2026-04-19 21:42:16 -0500

    feat: adding s3 policy resource

--- infra/main.tf
+++ infra/main.tf
@@ -108,3 +108,26 @@ resource "aws_security_group" "backend" {
     }
 }
 
+resource "aws_s3_bucket_policy" "frontend" {
+    bucket = aws_s3_bucket.frontend.id
+    
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Sid = "AllowCloudFrontAccess"
+                Effect = "Allow"
+                Principal = {
+                    Service = "cloudfront.amazonaws.com"
+                }
+                Action = "s3:GetObject"
+                Resoure = "${aws_s3_bucket.frontend.arn}/*"
+                Condition = {
+                    StringEquals = {
+                        "AWS:SourceArn" = aws_cloudfront_distribution.frontend.arn
+                    }
+                }
+            }
+        ]
+    })
+}
