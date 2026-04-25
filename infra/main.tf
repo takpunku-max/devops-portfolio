a6c6b672b1b3b59e826823d44eafe6b8ffe6e7f8
takpunku-max <takpunku@gmail.com>
2026-04-25 16:12:45 -0500

    feat: putting HTTP endpoint in front of frontend

--- infra/main.tf
+++ infra/main.tf
@@ -210,3 +210,41 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_apigatewayv2_api" "backend" {
+    name = "devops-portfolio-api"
+    protocol_type = "HTTP"
+
+    cors_configuration {
+        allow_origins = ["https://kjdevops-portfolio.com", "https://www.kjdevops-portfolio.com"]
+        allow_methods = ["GET", "POST", "OPTIONS"]
+        allow_headers = ["content-type"]
+    }
+}
+
+resource "aws_apigatewayv2_integration" "backend" {
+    api_id = aws_apigatewayv2_api.backend.id
+    integration_type = "AWS_PROXY"
+    integration_uri = aws_lambda_function.backend.invoke_arn
+    payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "backend" {
+    api_id = aws_apigatewayv2_api.backend.id
+    route_key = "ANY /{proxy+}"
+    target = "integrations/${aws_apigatewayv2_integration.backend.id}"
+}
+
+resource "aws_apigatewayv2_stage" "backend" {
+    api_id = aws_apigatewayv2_api.backend.id
+    name = "$default"
+    auto_deploy = true
+}
+
+resource "aws_lambda_permission" "backend" {
+    statement_id = "AllowAPIGatewayInvoke"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.backend.function_name
+    principal = "apigateway.amazonaws.com"
+    source_arn = "${aws_apigatewayv2_api.backend.execution_arn}/*/*"
+}
+
