d88a0c6ca26c5d9e8a05cf80bbd41593be1ad0ca
takpunku-max <takpunku@gmail.com>
2026-05-03 10:51:02 -0500

    feat: added cloudfront logging with aws-lambda-powertools

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,51 @@
-from fastapi import FastAPI # type: ignore
+from fastapi import FastAPI, Request # type: ignore
 from fastapi.middleware.cors import CORSMiddleware # type: ignore
 from mangum import Mangum
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools.logging import correlation_paths
+
+logger = Logger(service="devops-portfolio")
 
 app = FastAPI()
 
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-                   "https://kjdevops-portfolio.com",
-                   "https://www.kjdevops-portfolio.com"
-                   ],
-    allow_methods=["GET","POST","OPTIONS"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://kjdevops-portfolio.com",
+        "https://www.kjdevops-portfolio.com"
+    ],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["content-type"],
 )
 
 
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    logger.info(
+        "incoming request",
+        extra={
+            "path": request.url.path,
+            "method": request.method,
+        }
+    )
+    response = await call_next(request)
+    logger.info(
+        "request completed",
+        extra={
+            "path": request.url.path,
+            "method": request.method,
+            "status_code": response.status_code,
+        }
+    )
+    return response
+
+
 @app.get("/health")
 def health():
-    return {"status":"0K"}
+    logger.info("health check called")
+    return {"status": "0K"}
+
 
 handler = Mangum(app)
 
